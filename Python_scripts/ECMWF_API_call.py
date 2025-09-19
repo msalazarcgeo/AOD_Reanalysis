f8b20e61d93da750bc9a6ca821846335c82a2363
MSalazarCgeo <msalazar@centrogeo.edu.mx>
2025-09-19 13:20:35 -0600

    Change to get year from ECMWF

--- Python_scripts/ECMWF_API_call.py
+++ Python_scripts/ECMWF_API_call.py
@@ -6,19 +6,12 @@ dataset = "derived-era5-single-levels-daily-statistics"
 request = {
     "product_type": "reanalysis",
     "variable": [
-        "10m_u_component_of_wind",
-        "2m_dewpoint_temperature",
-        "2m_temperature",
-        "total_precipitation",
-        "100m_u_component_of_wind",
-        "100m_v_component_of_wind",
-        "total_column_rain_water",
         "boundary_layer_height"
     ],
     "year": "2022",
     "month": [
         "01", "02", "03",
-        "04", "05", "06",
+        "04", "05", "06"
         "07", "08", "09",
         "10", "11", "12"
     ],
@@ -38,10 +31,8 @@ request = {
     "daily_statistic": "daily_mean",
     "time_zone": "utc-06:00",
     "frequency": "1_hourly",
-    "area": [20.08, -99.67, 18.9, -98.5]
+    "area": [20.5, -99.67, 18.9, -98.5]
 }
 
 client = cdsapi.Client()
-key = '1f2d7087-7924-4ec6-8204-693ba1eeb2c0'
-
 client.retrieve(dataset, request).download()
