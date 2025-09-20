b25eaacb4ca914b7f773a3679e7622d6b1424c8e
MSalazarCgeo <msalazar@centrogeo.edu.mx>
2025-09-19 19:24:20 -0600

    The nothebook is ok to get all necesary data to run the R part

--- Python_scripts/ECMWF_API_call.py
+++ Python_scripts/ECMWF_API_call.py
@@ -1,17 +1,12 @@
 import cdsapi
 
-dataset = "derived-era5-single-levels-daily-statistics"
-
-
+dataset = "reanalysis-era5-single-levels"
 request = {
-    "product_type": "reanalysis",
-    "variable": [
-        "boundary_layer_height"
-    ],
-    "year": "2022",
+    "product_type": ["reanalysis"],
+    "year": ["2022"],
     "month": [
         "01", "02", "03",
-        "04", "05", "06"
+        "04", "05", "06",
         "07", "08", "09",
         "10", "11", "12"
     ],
@@ -28,11 +23,23 @@ request = {
         "28", "29", "30",
         "31"
     ],
-    "daily_statistic": "daily_mean",
-    "time_zone": "utc-06:00",
-    "frequency": "1_hourly",
+    "time": [
+        "00:00", "01:00", "02:00",
+        "03:00", "04:00", "05:00",
+        "06:00", "07:00", "08:00",
+        "09:00", "10:00", "11:00",
+        "12:00", "13:00", "14:00",
+        "15:00", "16:00", "17:00",
+        "18:00", "19:00", "20:00",
+        "21:00", "22:00", "23:00"
+    ],
+    "data_format": "grib",
+    "download_format": "unarchived",
+    "variable": ["boundary_layer_height"],
     "area": [20.5, -99.67, 18.9, -98.5]
 }
 
 client = cdsapi.Client()
-client.retrieve(dataset, request).download()
+
+
+client.retrieve(dataset, request, '2022_PBLH.grib')
